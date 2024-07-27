cc73d49eceda4a7bc564f2a1c60822d094ae83a7
Sanjay Kumar <sanjaykumar@Sanjays-MacBook-Air.local>
2024-07-27 17:26:14 +0530

    new changes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route for login , register
+Route::get('/', [LoginController::class, 'showLoginForm'])->name('login');
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
