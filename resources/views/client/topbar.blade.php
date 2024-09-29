5ec91009248f6fa8f86549e4ceb736b97efcfb17
Sanjay Kumar <sanjaykumar@Sanjays-MacBook-Air.local>
2024-09-29 13:27:44 +0530

    new changes

--- resources/views/client/topbar.blade.php
+++ resources/views/client/topbar.blade.php
@@ -1,62 +1,7 @@
 <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
-                <!-- Search -->
-                <div class="navbar-nav align-items-center">
-                  <div class="nav-item navbar-search-wrapper mb-0">
-                    <a class="nav-item nav-link search-toggler px-0" href="javascript:void(0);">
-                      <i class="bx bx-search-alt bx-sm"></i>
-                      <span class="d-none d-md-inline-block">Search (Ctrl+/)</span>
-                    </a>
-                  </div>
-                </div>
-                <!-- /Search -->
-
+               
                 <ul class="navbar-nav flex-row align-items-center ms-auto">
-                  <!-- Language -->
-                  <li class="nav-item dropdown-language dropdown me-2 me-xl-0">
-                    <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
-                      <i class="bx bx-globe bx-sm"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end">
-                      <li>
-                        <a
-                          class="dropdown-item"
-                          href="javascript:void(0);"
-                          data-language="en"
-                          data-text-direction="ltr">
-                          <span class="align-middle">English</span>
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          class="dropdown-item"
-                          href="javascript:void(0);"
-                          data-language="fr"
-                          data-text-direction="ltr">
-                          <span class="align-middle">French</span>
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          class="dropdown-item"
-                          href="javascript:void(0);"
-                          data-language="ar"
-                          data-text-direction="rtl">
-                          <span class="align-middle">Arabic</span>
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          class="dropdown-item"
-                          href="javascript:void(0);"
-                          data-language="de"
-                          data-text-direction="ltr">
-                          <span class="align-middle">German</span>
-                        </a>
-                      </li>
-                    </ul>
-                  </li>
-                  <!--/ Language -->
-
+                  
                   <!-- Style Switcher -->
                   <li class="nav-item dropdown-style-switcher dropdown me-2 me-xl-0">
                     <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
@@ -81,100 +26,6 @@
                     </ul>
                   </li>
                   <!-- / Style Switcher-->
-
-                  <!-- Quick links  -->
-                  <li class="nav-item dropdown-shortcuts navbar-dropdown dropdown me-2 me-xl-0">
-                    <a
-                      class="nav-link dropdown-toggle hide-arrow"
-                      href="javascript:void(0);"
-                      data-bs-toggle="dropdown"
-                      data-bs-auto-close="outside"
-                      aria-expanded="false">
-                      <i class="bx bx-grid-alt bx-sm"></i>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-end py-0">
-                      <div class="dropdown-menu-header border-bottom">
-                        <div class="dropdown-header d-flex align-items-center py-3">
-                          <h5 class="text-body mb-0 me-auto">Shortcuts</h5>
-                          <a
-                            href="javascript:void(0)"
-                            class="dropdown-shortcuts-add text-body"
-                            data-bs-toggle="tooltip"
-                            data-bs-placement="top"
-                            title="Add shortcuts"
-                            ><i class="bx bx-sm bx-plus-circle"></i
-                          ></a>
-                        </div>
-                      </div>
-                      <div class="dropdown-shortcuts-list scrollable-container">
-                        <div class="row row-bordered overflow-visible g-0">
-                          <div class="dropdown-shortcuts-item col">
-                            <span class="dropdown-shortcuts-icon bg-label-secondary rounded-circle mb-2">
-                              <i class="bx bx-calendar fs-4"></i>
-                            </span>
-                            <a href="app-calendar.html" class="stretched-link">Calendar</a>
-                            <small class="text-muted mb-0">Appointments</small>
-                          </div>
-                          <div class="dropdown-shortcuts-item col">
-                            <span class="dropdown-shortcuts-icon bg-label-secondary rounded-circle mb-2">
-                              <i class="bx bx-food-menu fs-4"></i>
-                            </span>
-                            <a href="app-invoice-list.html" class="stretched-link">Invoice App</a>
-                            <small class="text-muted mb-0">Manage Accounts</small>
-                          </div>
-                        </div>
-                        <div class="row row-bordered overflow-visible g-0">
-                          <div class="dropdown-shortcuts-item col">
-                            <span class="dropdown-shortcuts-icon bg-label-secondary rounded-circle mb-2">
-                              <i class="bx bx-user fs-4"></i>
-                            </span>
-                            <a href="app-user-list.html" class="stretched-link">User App</a>
-                            <small class="text-muted mb-0">Manage Users</small>
-                          </div>
-                          <div class="dropdown-shortcuts-item col">
-                            <span class="dropdown-shortcuts-icon bg-label-secondary rounded-circle mb-2">
-                              <i class="bx bx-check-shield fs-4"></i>
-                            </span>
-                            <a href="app-access-roles.html" class="stretched-link">Role Management</a>
-                            <small class="text-muted mb-0">Permission</small>
-                          </div>
-                        </div>
-                        <div class="row row-bordered overflow-visible g-0">
-                          <div class="dropdown-shortcuts-item col">
-                            <span class="dropdown-shortcuts-icon bg-label-secondary rounded-circle mb-2">
-                              <i class="bx bx-pie-chart-alt-2 fs-4"></i>
-                            </span>
-                            <a href="index.html" class="stretched-link">Dashboard</a>
-                            <small class="text-muted mb-0">User Profile</small>
-                          </div>
-                          <div class="dropdown-shortcuts-item col">
-                            <span class="dropdown-shortcuts-icon bg-label-secondary rounded-circle mb-2">
-                              <i class="bx bx-cog fs-4"></i>
-                            </span>
-                            <a href="pages-account-settings-account.html" class="stretched-link">Setting</a>
-                            <small class="text-muted mb-0">Account Settings</small>
-                          </div>
-                        </div>
-                        <div class="row row-bordered overflow-visible g-0">
-                          <div class="dropdown-shortcuts-item col">
-                            <span class="dropdown-shortcuts-icon bg-label-secondary rounded-circle mb-2">
-                              <i class="bx bx-help-circle fs-4"></i>
-                            </span>
-                            <a href="pages-faq.html" class="stretched-link">FAQs</a>
-                            <small class="text-muted mb-0">FAQs & Articles</small>
-                          </div>
-                          <div class="dropdown-shortcuts-item col">
-                            <span class="dropdown-shortcuts-icon bg-label-secondary rounded-circle mb-2">
-                              <i class="bx bx-window-open fs-4"></i>
-                            </span>
-                            <a href="modal-examples.html" class="stretched-link">Modals</a>
-                            <small class="text-muted mb-0">Useful Popups</small>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </li>
-                  <!-- Quick links -->
 
                   <!-- Notification -->
                   <li class="nav-item dropdown-notifications navbar-dropdown dropdown me-3 me-xl-2">
@@ -457,44 +308,24 @@
                       <li>
                         <a class="dropdown-item" href="pages-profile-user.html">
                           <i class="bx bx-user me-2"></i>
-                          <span class="align-middle">My Profile</span>
+                          <span class="align-middle">Your Profile</span>
                         </a>
                       </li>
+
+                      @if(getUserType()=='web')
                       <li>
-                        <a class="dropdown-item" href="pages-account-settings-account.html">
-                          <i class="bx bx-cog me-2"></i>
-                          <span class="align-middle">Settings</span>
-                        </a>
-                      </li>
-                      <li>
-                        <a class="dropdown-item" href="pages-account-settings-billing.html">
+                        <a class="dropdown-item" href="#">
                           <span class="d-flex align-items-center align-middle">
                             <i class="flex-shrink-0 bx bx-credit-card me-2"></i>
-                            <span class="flex-grow-1 align-middle">Billing</span>
+                            <span class="flex-grow-1 align-middle">Billing and Upgrade</span>
                             <span class="flex-shrink-0 badge badge-center rounded-pill bg-danger w-px-20 h-px-20"
                               >4</span
                             >
                           </span>
                         </a>
                       </li>
-                      <li>
-                        <div class="dropdown-divider"></div>
-                      </li>
-                      <li>
-                        <a class="dropdown-item" href="pages-faq.html">
-                          <i class="bx bx-help-circle me-2"></i>
-                          <span class="align-middle">FAQ</span>
-                        </a>
-                      </li>
-                      <li>
-                        <a class="dropdown-item" href="pages-pricing.html">
-                          <i class="bx bx-dollar me-2"></i>
-                          <span class="align-middle">Pricing</span>
-                        </a>
-                      </li>
-                      <li>
-                        <div class="dropdown-divider"></div>
-                      </li>
+                      @endif
+
                       <li>
                         <a class="dropdown-item" href="{{route('logout')}}" target="">
                           <i class="bx bx-power-off me-2"></i>
