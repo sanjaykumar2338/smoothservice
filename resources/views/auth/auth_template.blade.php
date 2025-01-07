0641ce4ad34a4b4ea8c9a52593257c3bcec43f19
Sanjay Kumar <sanjaykumar@Sanjays-MacBook-Air.local>
2025-01-07 23:25:24 +0530

    new changes

--- resources/views/auth/auth_template.blade.php
+++ resources/views/auth/auth_template.blade.php
@@ -127,7 +127,7 @@
           
             <p class="text-center">
               <span>New on our platform?</span>
-              <a href="{{route('register')}}">
+              <a href="{{route('workspace')}}">
                 <span>Create an account</span>
               </a>
             </p>
