7fc9cfc528fe3b65088c8d58cbc785550b342b9a
Sanjay Kumar <sanjaykumar@Sanjays-MacBook-Air.local>
2025-01-15 22:42:22 +0530

    new changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Facades\Config;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,10 +26,16 @@ class AppServiceProvider extends ServiceProvider
             'user' => 'App\Models\User', // Add other sender types here
         ]);
 
+        // Get the current request host
         $requestHost = request()->getHost();
+
+        // Extract the app's base host from the configured app URL
         $appHost = parse_url(config('app.url'), PHP_URL_HOST);
 
-        // Dynamically set SESSION_DOMAIN
-        config(['session.domain' => (filter_var($requestHost, FILTER_VALIDATE_IP) || $requestHost === $appHost) ? null : $requestHost]);
+        // Check if the request host is an IP address or matches the app's host
+        $isDefaultDomain = filter_var($requestHost, FILTER_VALIDATE_IP) || $requestHost === $appHost;
+
+        // Dynamically set the SESSION_DOMAIN
+        Config::set('session.domain', $isDefaultDomain ? null : '.' . $requestHost);
     }
 }
