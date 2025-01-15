cc624dad3d6ecb10b28d1394118481a01720749c
Sanjay Kumar <sanjaykumar@Sanjays-MacBook-Air.local>
2025-01-16 00:24:12 +0530

    new changes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -16,7 +16,6 @@ class LoginController extends Controller
     // Show the login form
     public function showLoginForm(Request $request)
     {
-        echo "test"; die;
         // Extract the host from the current request
         $host = $request->getHost();
 
